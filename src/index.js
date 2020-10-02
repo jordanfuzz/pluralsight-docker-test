17f6c0e1e7c2d79a217cd173daf0228f924cdfe9
Jordan Cooper <jordan@cooperplanet.com>
2020-10-02 00:06:10 -0700

    Add exclamation point (that'll fix it)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) => res.send('Hello world!!'))
 
 app.listen(port, () => console.log(`Listening on port ${80}`))
 
