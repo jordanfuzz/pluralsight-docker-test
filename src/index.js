49f07bf46d215138a3fbc2e8592d04e7e87ada3f
Jordan Cooper <jordan@cooperplanet.com>
2020-10-02 00:55:12 -0700

    Update text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) => res.send('Hello world v2!'))
 
 app.listen(port, () => console.log(`Listening on port ${8080}`))
 
